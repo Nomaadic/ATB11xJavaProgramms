c271350c6b8a9d76122c702fa6d81e8eb74e6065
Nomaadic <manikantakaramsetty42@gmail.com>
2025-03-06 11:36:51 +0530

    Changes made in print

--- src/ex_01_JavaPrograms/Lab001_HelloWorld.java
+++ src/ex_01_JavaPrograms/Lab001_HelloWorld.java
@@ -3,7 +3,7 @@ package ex_01_JavaPrograms;
 public class Lab001_HelloWorld {
     public static void main(String[] args)
     {
-        System.out.println("Hello Worlds");
+        System.out.println("Hello World");
     }
 
 }
