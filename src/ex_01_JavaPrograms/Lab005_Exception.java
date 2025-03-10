085bc4eac79197d8db150b26e500a206d94fa982
Nomaadic <manikantakaramsetty42@gmail.com>
2025-03-10 12:33:18 +0530

    Practice programs

---
+++ src/ex_01_JavaPrograms/Lab005_Exception.java
@@ -0,0 +1,8 @@
+package ex_01_JavaPrograms;
+
+public class Lab005_Exception {
+    public static void main(String[] args) {
+        // Exception message will be thrown
+            System.out.println(100/0);
+    }
+}
---
+++ src/ex_01_JavaPrograms/Lab006_PrintvsPrintln.java
@@ -0,0 +1,11 @@
+package ex_01_JavaPrograms;
+
+public class Lab006_PrintvsPrintln
+{
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.println("Hello");
+        System.out.print("New Print");
+        System.out.print("Statement");
+    }
+}
